d41448a86253999f652333ccc8d2c6b44dae4d58
IchikiRyo <tohodaiichiki@gmail.com>
2017-04-01 19:29:23 +0900

    部分テンプレートを用いました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
-    @reviews = current_user.reviews.page(params[:page]).per(10).order("created_at DESC")
+    @reviews = user.reviews.page(params[:page]).per(10).order("created_at DESC")
   end
 end
